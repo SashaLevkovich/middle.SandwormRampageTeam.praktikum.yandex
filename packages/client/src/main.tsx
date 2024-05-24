bd28f039aaa5c9ada9f219030ef69d89213cf05b
SashaLevkovich <aleksandr.levkovicha@gmail.com>
2024-05-24 09:44:18 +0300

    [SAN-11] Add scss

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import AppProviders from './app/appProviders'
-import './index.css'
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <AppProviders />
