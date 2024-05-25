7a9ee48bc230390eb6041d843232eb1eb19fffc3
Diana Pimenova <dpimenova@diasoft.ru>
2024-05-25 17:31:17 +0300

    Error handling inside the React-components

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import AppProviders from './app/appProviders'
 import './index.scss'
+import ErrorBoundary from 'shared/utils/error-boundary/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <AppProviders />
+  <ErrorBoundary>
+    <AppProviders />
+  </ErrorBoundary>
 )
