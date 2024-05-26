7d77857f0bf6ebe9e5695126c78fdf2fb73c7cd3
Anton Vlasov <antonvlasov@MacBook-Pro-MacBook.local>
2024-05-26 15:37:49 +0500

    review fix: removed /start route

--- packages/client/src/app/appRoutes.tsx
+++ packages/client/src/app/appRoutes.tsx
@@ -6,7 +6,7 @@ import { Layout } from 'features/Layout'
 
 import { NotFound } from 'pages/errors'
 import { Forum } from 'pages/forum'
-import { Game, GameStart } from 'pages/game'
+import { GameStart } from 'pages/game'
 import { Leaderboard } from 'pages/leaderboard'
 import { Profile } from 'pages/profile'
 import { SignIn } from 'pages/signIn'
@@ -18,8 +18,7 @@ export const appRouter = () =>
       element: <Layout bottomSlot={<Navbar />} />,
       path: '/',
       children: [
-        { index: true, element: <Game /> },
-        { path: '/start', element: <GameStart /> },
+        { index: true, element: <GameStart /> },
         {
           path: '/profile',
           element: <Profile />,
