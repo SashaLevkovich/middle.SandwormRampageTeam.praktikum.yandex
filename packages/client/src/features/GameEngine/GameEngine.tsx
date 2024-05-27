954b3a2c06e73f95109cc8537fd4cba60d69f9d3
Maksim Tsoy <maksimtsoy1990@mail.ru>
2024-05-27 10:55:32 +0300

    fix: styles conflicts

--- packages/client/src/features/GameEngine/GameEngine.tsx
+++ packages/client/src/features/GameEngine/GameEngine.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import styles from './gameEngine.module.scss'
+import styles from './GameEngine.module.scss'
 
 type Position = {
   x: number
