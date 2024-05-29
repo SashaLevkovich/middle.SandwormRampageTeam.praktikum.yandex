c0bd853fddd47665106ef41b5b1aac10ef97890f
Anton Vlasov <antonvlasov@MacBook-Pro-MacBook.local>
2024-05-29 14:13:32 +0500

    added scrollers for up and down the page

--- packages/client/src/pages/landing/Landing.tsx
+++ packages/client/src/pages/landing/Landing.tsx
@@ -1,14 +1,32 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import classes from './Landing.module.scss'
 import { Flex } from 'antd'
 import { Link } from 'react-router-dom'
 import { LANDING_PAGE_SECTIONS } from 'pages/landing/constants'
+import chevronDown from '../../shared/assets/icons/chevronDown.svg'
+import chevronUp from '../../shared/assets/icons/chevronUp.svg'
 
 export const Landing: FC = () => {
+  const [isBottomOfAPage, setIsBottomOfAPage] = useState(false)
+  const [isTopOfAPage, setIsTopOfAPage] = useState(true)
+  useEffect(() => {
+    const scrollHandler = () => {
+      setIsBottomOfAPage(
+        document.body.scrollHeight ===
+          window.innerHeight + Math.round(window.scrollY)
+      )
+      setIsTopOfAPage(window.scrollY <= window.innerHeight)
+    }
+    document.addEventListener('scroll', scrollHandler)
+    return () => {
+      document.removeEventListener('scroll', scrollHandler)
+    }
+  })
   return (
     <div className={classes.landingPage}>
       {LANDING_PAGE_SECTIONS.map(section => (
         <Flex
+          key={section.title}
           className={`${classes.landingPageSection} ${
             classes[section.className]
           }`}
@@ -20,6 +38,30 @@ export const Landing: FC = () => {
           <p className={classes.landingPageSectionText}>{section.text}</p>
         </Flex>
       ))}
+
+      {!isTopOfAPage && (
+        <Flex
+          className={classes.landingPageScrollerUp}
+          justify="center"
+          align="center"
+          onClick={() => {
+            window.scrollBy({ top: -window.innerHeight, behavior: 'smooth' })
+          }}>
+          <img src={chevronUp} />
+        </Flex>
+      )}
+
+      {!isBottomOfAPage && (
+        <Flex
+          className={classes.landingPageScrollerDown}
+          justify="center"
+          align="center"
+          onClick={() => {
+            window.scrollBy({ top: window.innerHeight, behavior: 'smooth' })
+          }}>
+          <img src={chevronDown} />
+        </Flex>
+      )}
 
       <Flex
         className={`${classes.landingPageSection} ${classes.landingPageSection5}`}
