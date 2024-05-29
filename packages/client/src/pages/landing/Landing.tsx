7bf1ee0f523088e5f0f89a61c72123012dc02686
Anton Vlasov <antonvlasov@MacBook-Pro-MacBook.local>
2024-05-29 11:44:36 +0500

    moved landing page seections into constants

--- packages/client/src/pages/landing/Landing.tsx
+++ packages/client/src/pages/landing/Landing.tsx
@@ -2,60 +2,24 @@ import { FC } from 'react'
 import classes from './Landing.module.scss'
 import { Flex } from 'antd'
 import { Link } from 'react-router-dom'
+import { LANDING_PAGE_SECTIONS } from 'pages/landing/constants'
 
 export const Landing: FC = () => {
   return (
     <div className={classes.landingPage}>
-      <Flex
-        className={`${classes.landingPageSection} ${classes.landingPageSection1}`}
-        justify="end"
-        align="end"
-        gap={23}
-        vertical>
-        <h1 className={classes.landingPageSectionTitle}>Worm</h1>
-        <p className={classes.landingPageSectionText}>Created by team 3</p>
-      </Flex>
-
-      <Flex
-        className={`${classes.landingPageSection} ${classes.landingPageSection2}`}
-        justify="start"
-        align="start"
-        gap={23}
-        vertical>
-        <h1 className={classes.landingPageSectionTitle}>About game</h1>
-        <p className={classes.landingPageSectionText}>
-          It’s basically a classic Snake but in the setting of Dune! Get your
-          sandworm ready, because you are about to save Arrakis from Spice
-          Harvesters.
-        </p>
-      </Flex>
-
-      <Flex
-        className={`${classes.landingPageSection} ${classes.landingPageSection3}`}
-        justify="start"
-        align="end"
-        gap={23}
-        vertical>
-        <h1 className={classes.landingPageSectionTitle}>LeaderBoards</h1>
-        <p className={classes.landingPageSectionText}>
-          You can compare your results with friends and other players. Try hard
-          if you want to be at the top!
-        </p>
-      </Flex>
-
-      <Flex
-        className={`${classes.landingPageSection} ${classes.landingPageSection4}`}
-        justify="start"
-        align="start"
-        gap={23}
-        vertical>
-        <h1 className={classes.landingPageSectionTitle}>Forum</h1>
-        <p className={classes.landingPageSectionText}>
-          You can also start a thread in our forum page. Don’t be a stranger,
-          talk to others and maybe get some insights about how to become the
-          best at our game.
-        </p>
-      </Flex>
+      {LANDING_PAGE_SECTIONS.map(section => (
+        <Flex
+          className={`${classes.landingPageSection} ${
+            classes[section.className]
+          }`}
+          justify={section.justify}
+          align={section.align}
+          gap={23}
+          vertical>
+          <h1 className={classes.landingPageSectionTitle}>{section.title}</h1>
+          <p className={classes.landingPageSectionText}>{section.text}</p>
+        </Flex>
+      ))}
 
       <Flex
         className={`${classes.landingPageSection} ${classes.landingPageSection5}`}
---
+++ packages/client/src/pages/landing/constants.ts
@@ -0,0 +1,30 @@
+export const LANDING_PAGE_SECTIONS = [
+  {
+    className: 'landingPageSection1',
+    title: 'Worm',
+    text: 'Created by team 3',
+    justify: 'end',
+    align: 'end',
+  },
+  {
+    className: 'landingPageSection2',
+    title: 'About game',
+    text: 'It’s basically a classic Snake but in the setting of Dune! Get your sandworm ready, because you are about to save Arrakis from Spice Harvesters.',
+    justify: 'start',
+    align: 'start',
+  },
+  {
+    className: 'landingPageSection3',
+    title: 'LeaderBoards',
+    text: 'You can compare your results with friends and other players. Try hard if you want to be at the top!',
+    justify: 'start',
+    align: 'end',
+  },
+  {
+    className: 'landingPageSection4',
+    title: 'Forum',
+    text: 'You can also start a thread in our forum page. Don’t be a stranger, talk to others and maybe get some insights about how to become the best at our game.',
+    justify: 'start',
+    align: 'start',
+  },
+]
