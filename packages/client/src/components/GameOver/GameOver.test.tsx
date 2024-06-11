3661ac7804fbcde26dfda9a9d470668db6974fca
Anton Vlasov <antonvlasov@MacBook-Pro-MacBook.local>
2024-06-11 10:58:05 +0500

    added test for GameEngine and refactored GameOver test

--- packages/client/src/components/GameOver/GameOver.test.tsx
+++ packages/client/src/components/GameOver/GameOver.test.tsx
@@ -5,9 +5,8 @@ import '@testing-library/jest-dom'
 describe('Testing GameOver component', () => {
   it('GameOver component renders scores correctly', () => {
     render(<GameOver score="228" />)
-    const h3 = screen.getAllByRole('heading')[1]
+    const scoreElement = screen.getByText('Score: 228')
 
-    expect(h3).toBeInTheDocument()
-    expect(h3.textContent).toBe('Score: 228')
+    expect(scoreElement).toBeInTheDocument()
   })
 })
