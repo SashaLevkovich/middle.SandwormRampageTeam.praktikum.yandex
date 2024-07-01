16d41c7a727a0e8add04f9d5fec8bde6c0a81a6c
Maksim Tsoy <maksimtsoy1990@mail.ru>
2024-07-01 20:10:33 +0300

    fix: import in jest config

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,5 +1,6 @@
-import dotenv from 'dotenv'
-dotenv.config()
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenv = require('dotenv');
+dotenv.config();
 
 export default {
   preset: 'ts-jest',
