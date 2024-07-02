7cf6fceaa0dcb8628f847b34c5b3de698980d500
Diana Pimenova <dpimenova@diasoft.ru>
2024-07-02 13:10:46 +0300

    Добавила serialize в скрипт

--- packages/client/server/index.ts
+++ packages/client/server/index.ts
@@ -5,6 +5,7 @@ dotenv.config()
 
 import express from 'express'
 import path from 'path'
+import serialize from 'serialize-javascript'
 
 const port = process.env.PORT || 80
 const clientPath = path.join(__dirname, '..')
@@ -70,14 +71,12 @@ async function createServer() {
       const { html: appHtml, initialState } = await render()
 
       // Заменяем комментарий на сгенерированную HTML-строку
-      const html = template
-        .replace(`<!--ssr-outlet-->`, appHtml)
-        .replace(
-          `<!--ssr-initial-state--> <div id="root"><!--ssr-outlet--></div>`,
-          `<script>window.APP_INITIAL_STATE = ${JSON.stringify(
-            initialState
-          )}</script>`
-        )
+      const html = template.replace(`<!--ssr-outlet-->`, appHtml).replace(
+        `<!--ssr-initial-state-->`,
+        `<script>window.APP_INITIAL_STATE = ${serialize(initialState, {
+          isJSON: true,
+        })}</script>`
+      )
 
       // Завершаем запрос и отдаём HTML-страницу
       res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
