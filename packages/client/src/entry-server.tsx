29d44686695254ff62bda45dbdb64407345855a0
Diana Pimenova <dpimenova@diasoft.ru>
2024-07-02 12:58:59 +0300

    Дополнила файл entry-server.tsx созданием стора

--- packages/client/src/entry-server.tsx
+++ packages/client/src/entry-server.tsx
@@ -1,5 +1,26 @@
 import { TestSSR } from 'pages/testSSR'
 import React from 'react'
 import ReactDOM from 'react-dom/server'
+import { configureStore } from '@reduxjs/toolkit'
+import { reducer } from 'app/redux/store'
+import { fetchUserThunk } from 'app/redux/slice/user'
+import { Provider } from 'react-redux'
+import AppProviders from 'app/appProviders'
 
-export const render = () => ReactDOM.renderToString(<TestSSR />)
+// export const render = () => ReactDOM.renderToString(<TestSSR />)
+export const render = async () => {
+  const store = configureStore({
+    reducer,
+  })
+
+  await store.dispatch(fetchUserThunk())
+
+  return {
+    html: ReactDOM.renderToString(
+      <Provider store={store}>
+        <AppProviders />
+      </Provider>
+    ),
+    initialState: store.getState(),
+  }
+}
