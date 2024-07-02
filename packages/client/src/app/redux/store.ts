eb10f6f09dba8fbcc3292c592d964a1752d6d84e
Diana Pimenova <dpimenova@diasoft.ru>
2024-07-02 12:39:10 +0300

    Инициализировала хранилище

--- packages/client/src/app/redux/store.ts
+++ packages/client/src/app/redux/store.ts
@@ -1,6 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { userApi } from './api'
 import rootReducer from './rootReducer'
+import {
+  useDispatch as useDispatchBase,
+  useSelector as useSelectorBase,
+  TypedUseSelectorHook,
+  useStore as useStoreBase,
+} from 'react-redux'
+
+export const reducer = combineReducers({})
 
 export const store = configureStore({
   reducer: rootReducer,
@@ -11,3 +19,7 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+
+export const useDispatch: () => AppDispatch = useDispatchBase
+export const useSelector: TypedUseSelectorHook<RootState> = useSelectorBase
+export const useStore: () => typeof store = useStoreBase
