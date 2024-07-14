a339afa5481e28e9195a92fcf0bc5dd541e4ab65
aleksandr-levkovich-gismart <alexander.levkovich@gismart.com>
2024-07-14 19:44:10 +0300

    Fix import

--- packages/client/src/pages/gameStart/GameStart.tsx
+++ packages/client/src/pages/gameStart/GameStart.tsx
@@ -1,16 +1,17 @@
+import { FullscreenExitOutlined, FullscreenOutlined } from '@ant-design/icons'
 import { FC, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { FullscreenExitOutlined, FullscreenOutlined } from '@ant-design/icons'
 
 import { authRequests, oAuthRequests } from 'app/api'
-import { setLocalStorageUser } from 'shared/utils/userLocalStorage'
-import { getOAuthCodeFromUrl } from 'shared/helpers/getOAuthCodeFromUrl'
-import { HttpStatuses } from 'shared/constants/httpStatuses'
+
 import linesImg from 'shared/assets/images/Lines.svg'
 import moonsImg from 'shared/assets/images/Moons.svg'
+import { HttpStatuses } from 'shared/constants/httpStatuses'
+import { getOAuthCodeFromUrl } from 'shared/helpers/getOAuthCodeFromUrl'
 
-import classes from './GameStart.module.scss'
 import { AxiosError } from 'axios'
+import { setLocalStorageUser } from 'shared/helpers/userLocalStorage'
+import classes from './GameStart.module.scss'
 
 export const GameStart: FC = () => {
   const [isFullScreen, setFullScreen] = useState(false)
