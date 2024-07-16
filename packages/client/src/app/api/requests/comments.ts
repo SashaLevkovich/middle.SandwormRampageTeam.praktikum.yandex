34062a93e21f1c842272f6a4777c4f8a8f0e7f36
Diana Pimenova <dpimenova@diasoft.ru>
2024-07-16 17:18:08 +0300

    Поправила апи комментов

--- packages/client/src/app/api/requests/comments.ts
+++ packages/client/src/app/api/requests/comments.ts
@@ -1,4 +1,5 @@
 import { rootApi } from 'app/api'
+import jwt from 'jsonwebtoken'
 
 export interface IComment {
   topicId: number
@@ -7,30 +8,52 @@ export interface IComment {
 
 class CommentsRequests {
   private static baseUrl: string
+  private static baseUrlTopics: string
+  private readonly token: string
 
   constructor() {
-    CommentsRequests.baseUrl = '/comments'
+    CommentsRequests.baseUrl = '/api/comments'
+    CommentsRequests.baseUrlTopics = '/api/topics'
+    this.token = jwt.sign({ id: 887 }, 'myjwtsecretkey', { expiresIn: '240h' })
   }
 
   getComments(commentId: number) {
     return rootApi.get<IComment>(
-      `${CommentsRequests.baseUrl}/${commentId}/comments`
+      `${CommentsRequests.baseUrlTopics}/${commentId}/comments`,
+      this.getHeaders()
     )
   }
 
   createComment(commentId: number, data: IComment) {
     return rootApi.post(
       `${CommentsRequests.baseUrl}/${commentId}/comment`,
-      data
+      data,
+      this.getHeaders()
     )
   }
 
   updateComment(id: number, data: IComment) {
-    return rootApi.put(`${CommentsRequests.baseUrl}/${id}`, data)
+    return rootApi.put(
+      `${CommentsRequests.baseUrl}/${id}`,
+      data,
+      this.getHeaders()
+    )
   }
 
   deleteComment(id: number) {
-    return rootApi.delete(`${CommentsRequests.baseUrl}/${id}`)
+    return rootApi.delete(
+      `${CommentsRequests.baseUrl}/${id}`,
+      this.getHeaders()
+    )
+  }
+
+  private getHeaders() {
+    return {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+        'Content-Type': 'application/json',
+      },
+    }
   }
 }
 
